039bde29bb02c4b633f4b2349fb544f946133b5d
WebersIO <muzammilmetar82@gmail.com>
2025-08-01 16:36:14 +0530

    completed

---
+++ src/components/Hero.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>
+      <div className="container-fluid px-0">
+        <div className="row">
+          <div id="particles-js" class="position-absolute top-0 mb-5 w-100"></div>
+          <div class="count-particles w-100">
+            <span class="js-count-particles">hhhh</span>
+          </div>
+
+          <div className="col-lg-6 border d-flex justify-content-center align-content-center"></div>
+          <div className="col-lg-6 border d-flex justify-content-center align-content-center">hii</div>
+        </div>
+        <div>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+
+export default Hero
